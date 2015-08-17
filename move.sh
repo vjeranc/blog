338cad1b3b7f6cc5c10723c3c7361d095d36381a
Vjeran Crnjak <vjeran.crnjak@gmail.com>
2015-08-17 13:49:26 +0200

    bugfix

--- move.sh
+++ move.sh
@@ -1,5 +1,8 @@
 stack build
 ./.stack-work/install/x86_64-osx/lts-2.20/7.8.4/bin/vjeranc rebuild
+cp ../vjeranc.github.io/CNAME _site
+cp ../vjeranc.github.io/README.md _site
+rm -rf ../vjeranc.github.io/*
 cp -R _site/* ../vjeranc.github.io
 cd ../vjeranc.github.io
 git add .
